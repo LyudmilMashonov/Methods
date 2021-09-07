c9abccac81cc42e6258e0ce87b3ab7a7006010d6
Anubiss <Anubiss@NOTEBOOK-LUBAKA>
2021-09-07 16:48:39 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SmallestOfThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            int min = GetMinimalSum(firstNumber, secondNumber, thirdNumber);
+            Console.WriteLine(min);
+        }
+
+        private static int GetMinimalSum(int firstNumber,  int secondNumber, int thirdNumber)
+        {
+            int min = Math.Min(Math.Min(firstNumber, secondNumber)
+                , thirdNumber);
+            return min;
+        }
+    }
+}
